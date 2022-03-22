8c542147b9d5215c1e729294efa9a314b665a062
Nebula <102142601+hereisnebula@users.noreply.github.com>
2022-03-22 15:38:23 +0700

    dipper: Shipped Quick-Tap feature.

--- lineage_dipper.mk
+++ lineage_dipper.mk
@@ -9,6 +9,9 @@ $(call inherit-product, device/xiaomi/dipper/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Quick-Tap
+TARGET_SUPPORTS_QUICK_TAP := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_dipper
 PRODUCT_DEVICE := dipper
